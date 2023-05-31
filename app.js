507132176ab0fdf9952d02fa6a8efa5479ffb596
Rajesh-0223 <rajeswararao0223@gmail.com>
2023-05-31 06:56:39 +0000

    Complete APIs

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ let db = null;
 
 const initializeDBAndServer = async () => {
   try {
-    db = open({
+    db = await open({
       filename: dbPath,
       driver: sqlite3.Database,
     });
@@ -63,7 +63,142 @@ app.get("/states/:stateId/", async (request, response) => {
         state_id = ${stateId};
     `;
   const state = await db.get(getStateQuery);
-  response.send(state);
+
+  response.send(convertStateToPascalCase(state));
+});
+
+const convertDistrictTableToPascalCase = (dbObject) => {
+  return {
+    districtId: dbObject.district_id,
+    districtName: dbObject.district_name,
+    stateId: dbObject.state_id,
+    cases: dbObject.cases,
+    cured: dbObject.cured,
+    active: dbObject.active,
+    deaths: dbObject.deaths,
+  };
+};
+
+// Create a district in the district table, district_id is auto-incremented
+app.post("/districts/", async (request, response) => {
+  const districtDetails = request.body;
+
+  const {
+    districtName,
+    stateId,
+    cases,
+    cured,
+    active,
+    deaths,
+  } = districtDetails;
+
+  const createNewDistrict = `
+        INSERT INTO 
+        district (district_name, state_id, cases, cured, active, deaths)
+        VALUES (
+            '${districtName}',
+            ${stateId},
+            ${cases},
+            ${cured},
+            ${active},
+            ${deaths}
+        );`;
+  const newDistrict = await db.run(createNewDistrict);
+  const districtId = newDistrict.lastID;
+  response.send("District Successfully Added");
+});
+
+// Returns a district based on the district ID
+app.get("/districts/:districtId/", async (request, response) => {
+  const { districtId } = request.params;
+
+  const getDistrictQuery = `
+        SELECT
+        *
+        FROM
+        district
+        WHERE 
+        district_id = ${districtId};`;
+  const district = await db.get(getDistrictQuery);
+  response.send(convertDistrictTableToPascalCase(district));
+});
+
+// Deletes a district from the district table based on the district ID
+app.delete("/districts/:districtId/", async (request, response) => {
+  const { districtId } = request.params;
+
+  const deleteDistrictQuery = `
+        DELETE FROM
+        district
+        WHERE
+        district_id = ${districtId};`;
+  await db.run(deleteDistrictQuery);
+  response.send("District Removed");
+});
+
+// Updates the details of a specific district based on the district ID
+app.put("/districts/:districtId/", async (request, response) => {
+  const { districtId } = request.params;
+  const districtDetails = request.body;
+
+  const {
+    districtName,
+    stateId,
+    cases,
+    cured,
+    active,
+    deaths,
+  } = districtDetails;
+
+  const updateDistrictQuery = `
+        UPDATE 
+        district
+        SET
+        district_name = '${districtName}',
+        state_id = ${stateId},
+        cases = ${cases},
+        cured = ${cured},
+        active = ${active},
+        deaths = ${deaths}
+        WHERE 
+        district_id = ${districtId};`;
+  await db.run(updateDistrictQuery);
+  response.send("District Details Updated");
+});
+
+// Returns the statistics of total cases, cured, active, deaths of a specific state based on state ID
+app.get("/states/:stateId/stats/", async (request, response) => {
+  const { stateId } = request.params;
+
+  const getStateStatsQuery = `
+        SELECT 
+        SUM(cases) as totalCases,
+        SUM(cured) as totalCured,
+        SUM(active) as totalActive,
+        SUM(deaths) as totalDeaths
+        FROM 
+        district
+        WHERE
+        state_id = ${stateId};`;
+  const stateStats = await db.get(getStateStatsQuery);
+  console.log(stateStats);
+  response.send(stateStats);
+});
+
+// Returns an object containing the state name of a district based on the district ID
+app.get("/districts/:districtId/details/", async (request, response) => {
+  const { districtId } = request.params;
+
+  const getStateNameQuery = `
+        SELECT
+        state_name as stateName
+        FROM 
+        district INNER JOIN state
+        ON state.state_id = district.state_id
+        WHERE 
+        district_id = ${districtId};`;
+  const stateName = await db.get(getStateNameQuery);
+  response.send(stateName);
 });
 
 module.exports = app;
